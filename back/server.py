218eaf0135b2f5e9635445cce7680c308405af01
fjcu256 <fjcu256@gmail.com>
2025-03-01 16:11:21 -0500

    Create backend server

---
+++ back/server.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app, origins="*")
+
+@app.route("/api/example", methods=['GET'])
+def example():
+    return jsonify(
+        {
+            "data": {
+                "score": 75,
+            }
+        }
+    )
+
+@app.route("/api/image", methods=['POST'])
+def image():
+    result = request.json
+    print(result['image'])
+
+    # TODO use model to get score
+
+    return jsonify(
+        {
+            "data": {
+                "score": 75
+            }
+        }
+    )
+
+if __name__ == "__main__":
+    # debug = True so we can see live updates while developing
+    app.run(debug=True, port=3000)
