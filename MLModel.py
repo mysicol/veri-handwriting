25ea8c22e676d75bccf1b2ab588ae63cf9cf1249
fjcu256 <fjcu256@gmail.com>
2025-03-01 14:54:07 -0500

    Adjust Logistic Regression model

--- MLModel.py
+++ MLModel.py
@@ -1,14 +1,12 @@
 import numpy
 from enum import Enum
 import random
+import math
 
 class Methods(Enum):
     DIRECT_LINEAR_REGRESSION = 0
-    ITERATIVE_LEAST_SQUARES = 1
-    DIRECT_LOGISTIC_REGRESSION = 2
-    GRADIENT_LOGISTIC_REGRESSION = 3
-    SVM = 4
-    KNN = 5
+    GRADIENT_LOGISTIC_REGRESSION = 1
+    SVM = 2
 
 
 class MLModel:
@@ -19,24 +17,17 @@ class MLModel:
         match self.__method:
             case Methods.DIRECT_LINEAR_REGRESSION:
                 self.__w = self.getDLRWeights()
-            case Methods.ITERATIVE_LEAST_SQUARES:
-                self.__w = self.getILSWeights(learningRate, epochs, epsilon)
-            case Methods.DIRECT_LOGISTIC_REGRESSION:
-                None
             case Methods.GRADIENT_LOGISTIC_REGRESSION:
-                None
+                self.__w = self.getGLRWeights(learningRate, epochs, epsilon)
             case Methods.SVM:
                 None
-            case Methods.KNN:
-                None
-        print(self.__w)
 
     def getDLRWeights(self):
         XwBias = numpy.append(numpy.ones((self.__X.shape[0], 1)), self.__X, axis = 1)
         w = numpy.linalg.inv((XwBias.T) @ XwBias) @ (XwBias.T) @ self.__Y
         return w
 
-    def getILSWeights(self, learningRate, epochs, epsilon):
+    def getGLRWeights(self, learningRate, epochs, epsilon):
         m = numpy.mean(self.__X, axis=0, keepdims=True)
         s = numpy.std(self.__X, axis=0, ddof=1, keepdims=True)
         XZscored = (self.__X - m)/s
@@ -50,11 +41,11 @@ class MLModel:
             else:
                 w = numpy.append(w, temp, axis = 0)
 
-        Yhat = XwBiasZscored @ w
+        Yhat = 1/(1 + math.e**(-(XwBiasZscored @ w)))
         for i in range(epochs):
-            dJdw = (2 / self.__Y.shape[0]) * ((XwBiasZscored.T) @ (Yhat - self.__Y))
+            dJdw = (1 / self.__Y.shape[0]) * ((XwBiasZscored.T) @ (Yhat - self.__Y))
             w = w - (learningRate * dJdw)
-            Yhat = XwBiasZscored @ w
+            Yhat = 1/(1 + math.e**(-(XwBiasZscored @ w)))
 
         return w
 
@@ -62,15 +53,9 @@ class MLModel:
         match self.__method:
             case Methods.DIRECT_LINEAR_REGRESSION:
                 return self.directLinearRegression(X)
-            case Methods.ITERATIVE_LEAST_SQUARES:
-                return self.iterativeLeastSquares(X)
-            case Methods.DIRECT_LOGISTIC_REGRESSION:
-                None
             case Methods.GRADIENT_LOGISTIC_REGRESSION:
-                None
+                return self.gradientLogisticRegression(X)
             case Methods.SVM:
-                None
-            case Methods.KNN:
                 None
 
     def directLinearRegression(self, X):
@@ -78,35 +63,20 @@ class MLModel:
         Yhat = XwBias @ self.__w
         return Yhat
 
-    def iterativeLeastSquares(self, X):
+    def gradientLogisticRegression(self, X):
         m = numpy.mean(X, axis=0, keepdims=True)
         s = numpy.std(X, axis=0, ddof=1, keepdims=True)
         XZscored = (X - m)/s
         XwBiasZscored = numpy.append(numpy.ones((XZscored.shape[0], 1)), XZscored, axis = 1)
-        Yhat = XwBiasZscored @ self.__w
+        Yhat = 1/(1 + math.e**(-(XwBiasZscored @ self.__w)))
         return Yhat
 
-# Direct Logistic Regression
-def directLogisticRegression(X, Y):
-    None
-
-# Gradient Logistic Regression
-def gradientLogisticRegression(X, Y):
-    None
-
-# SVM
-def svm(X, Y):
-    None
-
-# KNN
-def knn(X, Y):
-    None
 
 if __name__ == "__main__":
     X_TEST = numpy.array([[0,1,3],[8,1,1],[0,4,3],[1,2,2],[9,3,0]], dtype=float)
     Y_TEST = numpy.array([[1],[0],[1],[0],[0]], dtype=int)
 
-    model = MLModel(X_TEST, Y_TEST, Methods.ITERATIVE_LEAST_SQUARES)
+    model = MLModel(X_TEST, Y_TEST, Methods.GRADIENT_LOGISTIC_REGRESSION)
     Yhat = model.score(X_TEST)
     print("Yhat:", Yhat.shape)
     print(Yhat)
