7347136735245a87847840b395e8ce6688c425ea
Abigail Hatcher <ah3658@drexel.edu>
2025-03-01 12:43:06 -0500

    Loading in images, grid squares test image

--- process_image.py
+++ process_image.py
@@ -1,9 +1,27 @@
 from PIL import Image
 
-def load_image(filepath):
+def open_image(filepath):
     try: 
         return Image.open(filepath)
     except IOError:
         return None
 
-print(load_image("IMG_2099.jpg"))
+def grayscale_convert(image_file):
+    return image_file.convert("L")
+
+def load_image(image_file):
+    return image_file.load()
+
+def find_top_square():
+    print(len(image_pixels))
+
+image_file = open_image("IMG_2099.jpg")
+print(image_file)
+
+gray_image_file = grayscale_convert(image_file)
+print(gray_image_file)
+
+image_pixels = load_image(gray_image_file)
+print(image_pixels)
+
+find_top_square(image_pixels)
