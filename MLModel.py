0ac1a34cae3e29e666c0be9bd608ae3b43e9b05c
fjcu256 <fjcu256@gmail.com>
2025-03-01 15:22:31 -0500

    Implement SVM model

--- MLModel.py
+++ MLModel.py
@@ -20,7 +20,7 @@ class MLModel:
             case Methods.GRADIENT_LOGISTIC_REGRESSION:
                 self.__w = self.getGLRWeights(learningRate, epochs, epsilon)
             case Methods.SVM:
-                None
+                self.__w = self.getSVMWeights()
 
     def getDLRWeights(self):
         XwBias = numpy.append(numpy.ones((self.__X.shape[0], 1)), self.__X, axis = 1)
@@ -49,6 +49,16 @@ class MLModel:
 
         return w
 
+    def getSVMWeights(self):
+        m = numpy.mean(self.__X, axis=0, keepdims=True)
+        s = numpy.std(self.__X, axis=0, ddof=1, keepdims=True)
+        XZscored = (self.__X - m)/s
+        XwBiasZscored = numpy.append(numpy.ones((XZscored.shape[0], 1)), XZscored, axis = 1)
+        YNegPos = 2 * self.__Y - 1
+        alpha = numpy.linalg.pinv(numpy.diag(YNegPos.T[0]) @ XwBiasZscored @ (XwBiasZscored.T) @ numpy.diag(YNegPos.T[0])) @ numpy.ones((XwBiasZscored.shape[0], 1))
+        w = (XwBiasZscored.T) @ numpy.diag(YNegPos.T[0]) @ alpha
+        return w
+
     def score(self, X):
         match self.__method:
             case Methods.DIRECT_LINEAR_REGRESSION:
@@ -56,7 +66,7 @@ class MLModel:
             case Methods.GRADIENT_LOGISTIC_REGRESSION:
                 return self.gradientLogisticRegression(X)
             case Methods.SVM:
-                None
+                return self.SVM(X)
 
     def directLinearRegression(self, X):
         XwBias = numpy.append(numpy.ones((X.shape[0], 1)), X, axis = 1)
@@ -71,12 +81,41 @@ class MLModel:
         Yhat = 1/(1 + math.e**(-(XwBiasZscored @ self.__w)))
         return Yhat
 
+    def SVM(self, X):
+        m = numpy.mean(X, axis=0, keepdims=True)
+        s = numpy.std(X, axis=0, ddof=1, keepdims=True)
+        XZscored = (X - m)/s
+        XwBiasZscored = numpy.append(numpy.ones((XZscored.shape[0], 1)), XZscored, axis = 1)
+        Yhat = XwBiasZscored @ self.__w
+        return Yhat
+
 
 if __name__ == "__main__":
     X_TEST = numpy.array([[0,1,3],[8,1,1],[0,4,3],[1,2,2],[9,3,0]], dtype=float)
     Y_TEST = numpy.array([[1],[0],[1],[0],[0]], dtype=int)
 
+    # Direct Linear Regression
+    model = MLModel(X_TEST, Y_TEST, Methods.DIRECT_LINEAR_REGRESSION)
+    Yhat = model.score(X_TEST)
+    print("Yhat:", Yhat.shape)
+    print(Yhat)
+    print()
+    print("Y_TEST:", Y_TEST.shape)
+    print(Y_TEST)
+    print()
+
+    # Gradient Logistic Regression
     model = MLModel(X_TEST, Y_TEST, Methods.GRADIENT_LOGISTIC_REGRESSION)
+    Yhat = model.score(X_TEST)
+    print("Yhat:", Yhat.shape)
+    print(Yhat)
+    print()
+    print("Y_TEST:", Y_TEST.shape)
+    print(Y_TEST)
+    print()
+
+    # SVM
+    model = MLModel(X_TEST, Y_TEST, Methods.SVM)
     Yhat = model.score(X_TEST)
     print("Yhat:", Yhat.shape)
     print(Yhat)
