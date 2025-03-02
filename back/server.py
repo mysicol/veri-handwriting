27e21e44c21725c8dcd09db07fc95790b5ecdd7b
fjcu256 <fjcu256@gmail.com>
2025-03-01 21:21:33 -0500

    Add basic UI structure for Statistics Page

--- back/server.py
+++ back/server.py
@@ -4,16 +4,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app, origins="*")
 
-@app.route("/api/example", methods=['GET'])
-def example():
-    return jsonify(
-        {
-            "data": {
-                "score": 75,
-            }
-        }
-    )
-
 @app.route("/api/image", methods=['POST'])
 def image():
     result = request.json
@@ -21,10 +11,33 @@ def image():
 
     # TODO use model to get score
 
+    worst = [
+        {
+            "id": 0,
+            "letter": "t",
+            "score": 45,    
+        },
+        {
+            "id": 1,
+            "letter": "q",
+            "score": 38,    
+        },
+        {
+            "id": 2,
+            "letter": "x",
+            "score": 25,    
+        },
+    ]
+
     return jsonify(
         {
             "summary": {
-                "score": 75
+                "text": "This is a really long summary about how bad your handwriting is. It really is bad. You should seek help. No one can read your handwriting. You should give up on all your hopes and dreams. No one could love you. Womp womp.",
+                "neatness": 75,
+                "consistency": 55,
+                "mostLegible": "a",
+                "mostConsistent": "j",
+                "worst": worst,
             }
         }
     )
--- front/src/Components/WriteRight.jsx
+++ front/src/Components/WriteRight.jsx
@@ -3,10 +3,21 @@ import axios from "axios";
 import "./WriteRight.css";
 
 export default function WriteRight() {
-  let [form, setForm] = useState({ image: "test" });
+  let [form, setForm] = useState({ image: "" });
   let [formSent, setFormSent] = useState(false);
   let [summary, setSummary] = useState({
-    score: 0,
+    text: "",
+    neatness: 0,
+    consistency: 0,
+    mostLegible: "",
+    mostConsistent: "",
+    worst: [
+      {
+        id: 0,
+        letter: "",
+        score: 0,
+      },
+    ],
   }); // default data
 
   let sendForm = async function () {
@@ -18,10 +29,45 @@ export default function WriteRight() {
   };
 
   let summaryPage = (
-    <div className="summary-container">
-      <div id="summary">
-        <h1 className="header">Write Right</h1>
-        <div className="score">Score: {summary.score}%</div>
+    <div id="statistics">
+      <h1 className="header">Here are your statistics!</h1>
+      <div className="content">
+        <div className="feedback">
+          <div className="summary">
+            <h2>Summary:</h2>
+            <div>{summary.text}</div>
+          </div>
+          <div className="scores">
+            <h3>Score:</h3>
+            <div className="neatness">Neatness: {summary.neatness}%</div>
+            <div className="consistency">
+              Consistency: {summary.consistency}%
+            </div>
+          </div>
+          <div className="worst">
+            <h3>Letters to work on:</h3>
+            <table className="worst-list">
+              <tr>
+                <th>Letter</th>
+                <th>Legibility</th>
+              </tr>
+              {summary.worst.map(({ id, letter, score }) => (
+                <tr key={id} className="worst-item">
+                  <td>{letter}</td>
+                  <td>{score}%</td>
+                </tr>
+              ))}
+            </table>
+          </div>
+        </div>
+        <div className="special">
+          <div className="most-legible">
+            Most Legible: {summary.mostLegible}
+          </div>
+          <div className="most-consistent">
+            Most Consistent: {summary.mostConsistent}
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -29,7 +75,13 @@ export default function WriteRight() {
   let formPage = (
     <div className="form-container">
       <div id="form">
-        <h1>Submit Image</h1>
+        <h1>Welcome to Write Right</h1>
+        <h2>
+          Get started by submitting a photo of your beautiful handwriting!
+        </h2>
+        <h3>**Try to keep only one letter per box :)</h3>
+        <input type="file" />
+        <img src={form.image} />
         <button className="submit-button" onClick={sendForm}>
           Submit
         </button>
